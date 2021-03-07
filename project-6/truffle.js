61448d483f7e55b9a780a2c9609bc8c8f6301c5e
Raúl López <hi@raullg.com>
2021-03-07 22:23:34 +0000

    Add websocket and connect to Ganache GUI

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -7,8 +7,9 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // Match any network id
+      port: 7545,
+      network_id: "*", // Match any network id
+      websockets: true
     }
   }
 };
